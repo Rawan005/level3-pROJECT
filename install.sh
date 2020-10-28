6ddca2f809953158763602450e5e15e00e35b861
Ubuntu <ubuntu@ip-10-0-1-216.me-south-1.compute.internal>
2020-10-28 13:56:05 +0000

    Deplo carts

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ sudo apt install -y gnupg
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3EFE0E0A2F2F60AA
 echo "deb http://ppa.launchpad.net/tektoncd/cli/ubuntu eoan main"|sudo tee /etc/apt/sources.list.d/tektoncd-ubuntu-cli.list
 sudo apt update && sudo apt install -y tektoncd-cli
+echo "Ingress: install" | tee -a output.log
+	kubectl apply -n ingress-nginx -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-0.32.0/deploy/static/provider/cloud/deploy.yaml | tee -a output.log
+	kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
